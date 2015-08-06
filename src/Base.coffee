noflo/noflo-runtime-base

commit 8312c305c32456b8c7ef892350d7f655a3f2daf4

Network: Send started message on start(), don't wait till running
When sending network:start to a network without IIPs, we would
never get a response, because data was not flowing
(network is not running, just started).
Caused issue in fbp-spec where we were waiting for network start
before starting test (sending data on exported inports)

src/Base.coffee
@@ -21,7 +21,7 @@ class BaseTransport
       path = 'default/main'
       @context = 'none'
       @graph.registerGraph path, @options.defaultGraph
-      @network.initNetwork @options.defaultGraph, { graph: path }, @context
+      @network.initNetwork @options.defaultGraph, { graph: path }, @context, () ->
 
     if @options.captureOutput? and @options.captureOutput
       # Start capturing so that we can send it to the UI when it connects

src/protocol/Network.coffee
@@ -54,6 +54,14 @@ getConnectionSignature = (connection) ->
 getSocketSignature = (socket) ->
   return getConnectionSignature(socket.from) +  ' -> ' + getConnectionSignature(socket.to)
 
+networkIsRunning = (net) ->
+  # compat with old NoFlo
+  if net.isRunning
+    isRunning = net.isRunning()
+  else
+    isRunning = net.isStarted() and net.connectionCount > 0
+  return isRunning
+
 class NetworkProtocol extends EventEmitter
   constructor: (@transport) ->
     @networks = {}
@@ -113,7 +121,7 @@ class NetworkProtocol extends EventEmitter
     sign = getSocketSignature(event.socket)
     return @networks[graph].filters[sign]
 
-  initNetwork: (graph, payload, context) ->
+  initNetwork: (graph, payload, context, callback) ->
 
     # Ensure we stop previous network
     if @networks[payload.graph] and @networks[payload.graph].network
@@ -137,7 +145,7 @@ class NetworkProtocol extends EventEmitter
 
       # Run the network
       network.connect ->
-        network.start()
+        return callback null
     , opts
 
   subscribeNetwork: (network, payload, context) ->
@@ -185,11 +193,31 @@ class NetworkProtocol extends EventEmitter
       , context
 
   startNetwork: (graph, payload, context) ->
+    doStart = (net) =>
+      net.start()
+      if net.isStarted()
+        @sendAll 'started',
+          time: new Date
+          graph: payload.graph
+          running: networkIsRunning net
+          started: true
+        , context
+      else
+        @sendAll 'stopped',
+          time: new Date
+          graph: payload.graph
+          running: networkIsRunning net
+          started: false
+        , context
+
     network = @networks[payload.graph]
     if network and network.network
-      network.network.start()
+      # already initialized
+      doStart network.network
     else
-      @initNetwork graph, payload, context
+      @initNetwork graph, payload, context, () =>
+        network = @networks[payload.graph]
+        doStart network.network
 
   stopNetwork: (graph, payload, context) ->
     return unless @networks[payload.graph]
@@ -206,13 +234,9 @@ class NetworkProtocol extends EventEmitter
   getStatus: (graph, payload, context) ->
     network = @networks[payload.graph]
     return unless network
-    if network.network.isRunning
-      isRunning = network.network.isRunning()
-    else
-      isRunning = network.network.isStarted() and network.network.connectionCount > 0
     @send 'status',
         graph: payload.graph
-        running: isRunning
+        running: networkIsRunning network.network
         started: network.network.isStarted()
     , context
 

commit 530834c320bbdeaa6b4ab440963a1f2ec841db6f

Base: Call startNetwork() instead of initNetwork() for default graph
initNetwork does not do network.start() since commit 8312c305c324,
which caused default graphs to never start

src/Base.coffee
@@ -21,7 +21,7 @@ class BaseTransport
       path = 'default/main'
       @context = 'none'
       @graph.registerGraph path, @options.defaultGraph
-      @network.initNetwork @options.defaultGraph, { graph: path }, @context, () ->
+      @network.startNetwork @options.defaultGraph, { graph: path }, @context
 
     if @options.captureOutput? and @options.captureOutput
       # Start capturing so that we can send it to the UI when it connects