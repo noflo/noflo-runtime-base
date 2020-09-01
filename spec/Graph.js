3131e290f97210ffcec5834bd311f7d4ea5ddd22
Henri Bergius <henri.bergius@iki.fi>
2020-09-01 13:13:42 +0200

    Some linting

--- spec/Graph.js
+++ spec/Graph.js
@@ -14,14 +14,14 @@ describe('Graph protocol', () => {
     client = new direct.Client(runtime);
     client.connect();
     client2 = new direct.Client(runtime);
-    return client2.connect();
+    client2.connect();
   });
   afterEach(() => {
     client.disconnect();
     client = null;
     client2.disconnect();
     client2 = null;
-    return runtime = null;
+    runtime = null;
   });
 
   describe('sending graph:clear', () => {
@@ -33,9 +33,9 @@ describe('Graph protocol', () => {
       client.once('message', (msg) => {
         chai.expect(msg.protocol).to.equal('graph');
         chai.expect(msg.command).to.equal('error');
-        return done();
+        done();
       });
-      return client.send('graph', 'clear', payload);
+      client.send('graph', 'clear', payload);
     });
     it('should respond with graph:clear', (done) => {
       const payload = {
@@ -48,11 +48,11 @@ describe('Graph protocol', () => {
         chai.expect(msg.command).to.equal('clear');
         chai.expect(msg.payload).to.include.keys('id');
         chai.expect(msg.payload.id).to.equal(payload.id);
-        return done();
+        done();
       });
-      return client.send('graph', 'clear', payload);
+      client.send('graph', 'clear', payload);
     });
-    return it('should send to all clients', (done) => {
+    it('should send to all clients', (done) => {
       const payload = {
         id: 'mygraph',
         main: true,
@@ -63,13 +63,13 @@ describe('Graph protocol', () => {
         chai.expect(msg.command).to.equal('clear');
         chai.expect(msg.payload).to.include.keys('id');
         chai.expect(msg.payload.id).to.equal(payload.id);
-        return done();
+        done();
       });
-      return client.send('graph', 'clear', payload);
+      client.send('graph', 'clear', payload);
     });
   });
 
-  return describe('sending graph:addnode', () => {
+  describe('sending graph:addnode', () => {
     const graph = 'graphwithnodes';
     const payload = {
       id: 'node1',
@@ -84,23 +84,25 @@ describe('Graph protocol', () => {
       chai.expect(msg.protocol).to.equal('graph');
       chai.expect(msg.command).to.equal('addnode');
       chai.expect(msg.payload).to.deep.equal(payload);
-      return done();
+      done();
     };
     after(() => runtimeEvents = []);
     it('should respond with graph:addnode', (done) => {
       client.on('message', (msg) => checkAddNode(msg, done));
       client.send('graph', 'clear', { id: graph, main: true, secret: 'foo' });
-      return client.send('graph', 'addnode', authenticatedPayload);
+      client.send('graph', 'addnode', authenticatedPayload);
     });
     it('should have emitted an updated event', () => {
       chai.expect(runtimeEvents.length).to.equal(1);
       const event = runtimeEvents.shift();
-      return chai.expect(event.name).to.equal(graph);
+      chai.expect(event.name).to.equal(graph);
     });
-    return it('should send to all clients', (done) => {
+    it('should send to all clients', (done) => {
       client2.on('message', (msg) => checkAddNode(msg, done));
       client.send('graph', 'clear', { id: graph, main: true, secret: 'foo' });
-      return client.send('graph', 'addnode', authenticatedPayload);
+      client.send('graph', 'addnode', authenticatedPayload);
     });
   });
+
+
 });
